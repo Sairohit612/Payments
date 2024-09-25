0928cdcb87f4d3ee563ba7408421a0cd3e6a7b27
Sairohit612 <spysherly@gmail.com>
2024-09-25 10:40:50 +0530

    Update db.js with account schema

--- backend/db.js
+++ backend/db.js
@@ -12,6 +12,20 @@ const userSchema = mongoose.schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = {
-    User 
-} 
+const accountSchema = mongoose.Schema({
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true},
+    
+    balance: {
+    type: Number,
+    required:true}
+    });
+    
+    const Account = mongoose.model('Account', accountSchema);
+    
+    module.exports = {
+    User,
+    Account
+    };
